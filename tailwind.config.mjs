9461850d628e7d63059b08da2af5e48e2aebb653
Charlie Coppinger <charles.coppinger@gmail.com>
2024-05-21 17:56:03 +1200

    add jakarta sans

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -59,7 +59,7 @@ const config = {
                 sm: "calc(var(--radius) - 4px)",
             },
             fontFamily: {
-                sans: [...fontFamily.sans],
+                sans: ["Plus Jakarta Sans Variable", ...fontFamily.sans],
             },
         },
     },
