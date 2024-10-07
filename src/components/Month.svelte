a438eb2da4571bf7846468142b49a14cc5dc5a86
Charlie Coppinger <charles.coppinger@gmail.com>
2024-10-07 16:32:41 +1300

    increase testimonial image resolution, fix responsive issue and componentize month availbility item

---
+++ src/components/Month.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let availability: boolean
+</script>
+
+<div
+    class="flex xl:items-center flex-col xl:flex-row justify-center gap-2 xl:gap-3 px-4 cols-span-1 py-4 xl:py-0"
+>
+    <div
+        class="rounded-[1px] border border-neutral-300 w-[20px] h-[12px]"
+    >
+    {#if !availability}    
+        <div
+            class="border border-neutral-50 bg-red-400 w-full h-full rounded-[2px]"
+        >
+        </div>
+    {:else}
+        <div
+            class="border border-neutral-50 bg-green-500 w-full h-full rounded-[2px]"
+        >
+        </div>
+    {/if}
+    </div>
+    <p class={availability ? '' : 'line-through'}><slot/></p>
+</div>
